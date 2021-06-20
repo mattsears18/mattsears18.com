67dccdd9b25b5121690c368adab37edb50726f2a
mattsears18 <mattsears18@gmail.com>
2021-06-20 16:37:58 -0400

    add resume download

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import resumeIcon from '../public/images/resumeIcon80.png';
 import linkedInIcon from '../public/images/linkedInIcon80.png';
 import githubIcon from '../public/images/githubIcon80.png';
 import researchGateIcon from '../public/images/researchGateIcon80.png';
@@ -31,15 +32,31 @@ export default function Home() {
         <p
         // className={styles.linkedInIcon}
         >
-          <a href="https://github.com/mattsears18/">
+          <a href="/Matt Sears - Resume 2021-06-20.pdf" target="_blank">
+            <Image src={resumeIcon} alt="Download Resume" />
+          </a>
+          &nbsp;&nbsp;
+          <a
+            href="https://github.com/mattsears18/"
+            target="_blank"
+            rel="noreferrer"
+          >
             <Image src={githubIcon} alt="Github Profile" />
           </a>
           &nbsp; &nbsp;
-          <a href="https://www.researchgate.net/profile/Matt-Sears/research">
+          <a
+            href="https://www.researchgate.net/profile/Matt-Sears/research"
+            target="_blank"
+            rel="noreferrer"
+          >
             <Image src={researchGateIcon} alt="ResearchGate Profile" />
           </a>
           &nbsp; &nbsp;
-          <a href="https://linkedin.com/in/mattsears18/">
+          <a
+            href="https://linkedin.com/in/mattsears18/"
+            target="_blank"
+            rel="noreferrer"
+          >
             <Image src={linkedInIcon} alt="LinkedIn Profile" />
           </a>
         </p>
