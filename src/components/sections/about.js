3fad52cda98936195b3cb538cc51df51c44423e7
Matt Sears <mattsears18@gmail.com>
2022-03-26 13:55:07 -0400

    cleanup

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -171,24 +171,7 @@ const About = () => {
               construction professionals. I automated a ton of my design work as a civil engineer,
               and I spent four years building CLI apps and web apps for conducting construction
               productivity research to get my PhD.
-              {/* Fast-forward to today, and I've had the privilege of
-              working at <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients. */}
             </p>
-
-            {/* <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
-            </p> */}
 
             <p>
               Here are a few technologies that I've been working with lately (see{' '}
